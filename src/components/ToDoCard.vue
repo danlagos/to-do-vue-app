5c2e7728b10be8af100b1eb8dc16b117a019ae37
danlagos <lagos.d@gmail.com>
2020-12-04 15:22:22 -0800

    first commit, basic functionality, hard coded.

---
+++ src/components/ToDoCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="to-do">
+    <span>{{ todo.title }}: {{ todo.description }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDos",
+  props: {
+    todo: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.to-do {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.to-do:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="todo-Card">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ToDoCard v-for="todo in todos" :key="todo.id" :todo="todo" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToDoCard from "@/components/ToDoCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ToDoCard,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 5928101,
+          category: "learning",
+          title: "Study Vue",
+          description: "Start the Real Word View Course",
+          location: "San Diego",
+          date: "January 28, 2022",
+          time: "12:00",
+          complete: false,
+        },
+        {
+          id: 4582797,
+          category: "training",
+          title: "Run 3 miles",
+          description: "Run regular route",
+          location: "San Diego",
+          date: "January 28, 2022",
+          time: "12:00",
+          complete: false,
+        },
+        {
+          id: 8419988,
+          category: "training",
+          title: "learn to make curry paste",
+          description: "learn to make curry paste",
+          location: "San Diego",
+          date: "January 28, 2022",
+          time: "12:00",
+          complete: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.todo-Card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
