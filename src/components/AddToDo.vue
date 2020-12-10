d4991055d1317b936b13190f2c0b71e27b04ea25
danlagos <lagos.d@gmail.com>
2020-12-10 12:14:21 -0800

    styling add todo field and button.

--- src/components/AddToDo.vue
+++ src/components/AddToDo.vue
@@ -1,16 +1,27 @@
 <template>
-  <input class="field" type="field" name="field" id="field" />
-  <label for="field"></label>
-  <input type="submit" value="add" />
+  <div class="div-add-todo">
+    <input class="add-todo" type="field" name="field" id="field" />
+    <label for="field"></label>
+    <input class="btn-add-todo" type="submit" value="add" />
+  </div>
 </template>
 
 <script></script>
 
 <style scoped>
-.field {
+.div-add-todo {
+  display: inline-flex;
+}
+
+.btn-add-todo {
+  height: 2.25rem;
+}
+
+.add-todo {
   /* padding: 20px;*/
   width: 500px;
   margin: 5px;
+  height: 1.5rem;
   /* min-width: 500px;
   cursor: pointer;
   border: 1px solid #39495c;
